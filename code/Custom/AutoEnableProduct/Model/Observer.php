d76514c2ed8d5a1b5b542bc9d0461dc055fe86dc
Ngoc Ky <ngockya98@gmail.com>
2019-12-25 10:16:54 +0700

    commit

---
+++ code/Custom/AutoEnableProduct/Model/Observer.php
@@ -0,0 +1,18 @@
+<?php
+class Custom_AutoEnableProduct_Model_Observer
+{
+    public function salesOrderSaveAfter($observer)
+    {
+        $storeId = 0; //the admin store view, change this if you want to disable only for the store view from which the order came
+        $order= $observer->getEvent()->getOrder();
+
+        foreach ($order->getItemsCollection() as $item) {
+            $stockQty = (int)Mage::getModel('cataloginventory/stock_item')->loadByProduct($item->getProductId())->getQty();
+
+            if ($stockQty == 0) {
+                Mage::getModel('catalog/product_status')->updateProductStatus($item->getProductId(), $storeId, Mage_Catalog_Model_Product_Status::STATUS_DISABLED);
+            }
+        }
+
+    }
+}
